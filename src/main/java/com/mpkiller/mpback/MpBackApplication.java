3b435ff4f031ee0884cb7f0ad22b38e7bdd89be3
ZocP <87646631+ZocP@users.noreply.github.com>
2025-02-19 18:41:00 -0600

    <init>

---
+++ src/main/java/com/mpkiller/mpback/MpBackApplication.java
@@ -0,0 +1,13 @@
+package com.mpkiller.mpback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mpkiller/mpback/MpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpkiller.mpback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
